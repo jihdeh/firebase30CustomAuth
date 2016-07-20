a8d945a1a60392da069f2d396e2ba2f86207286c
Jihdeh <jide.b.tade@gmail.com>
2016-07-20 03:32:37 +0100

    Initial commit

---
+++ public/js/firebase-config.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+// TODO: Replace apiKey with your apps browser api key found here : 
+  //https://console.developers.google.com/apis/credentials
+//TODO: Replace authDomain with your website url and also update the redirect domain found at
+  //https://console.firebase.google.com/project/umbrella77-3bf3e/authentication/providers
+// TODO: Replace databaseURL with your databaseID can be found at
+  // https://console.firebase.google.com/project/umbrella77-3bf3e/settings/database
+var config = {
+  apiKey: "AIzaSyAvR1_S-5AIkdsUWCcP1XN5NxSl-LRtBfs",
+  // authDomain: "umbrella77-3bf3e.firebaseapp.com", 
+  databaseURL: "https://umbrella77-3bf3e.firebaseio.com"
+};
+firebase.initializeApp(config);
